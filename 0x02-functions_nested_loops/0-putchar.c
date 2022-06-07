3b34a883ce4a2ab3884865a671232413cf5e2e0f
Melaku <melakuwondwossen111@gmail.com>
2022-06-07 16:37:22 +0000

    Initial commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+
+/* Hello this is my comment and i would do as i please with it
+
+*/
+
+int main(){
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("/n");
+
+}
