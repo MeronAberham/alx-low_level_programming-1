1adabb707f47a5596b8880d03d0ccca0e0525e95
Melaku <melakuwondwossen111@gmail.com>
2022-06-08 02:00:39 +0300

    Initial commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,3 +10,8 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
+void natural(void);
+void fibonacci(void);
+void fibonacci(void);
+void fibonacci(void);
