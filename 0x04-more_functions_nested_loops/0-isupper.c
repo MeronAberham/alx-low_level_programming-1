350b45fd754800e191c4c1f7388151cb02bf0dcb
Melaku <melakuwondwossen111@gmail.com>
2022-06-09 21:24:27 +0300

    Initial Commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*
+  This code checks if a letter is upper or not upper 
+  x is the number to be checked
+  and if the letter is upper then it returns 1 else 0;
+ */
+ 
+int _isupper (int c) 
+{
+	if (c.isupper)
+	{
+	      return(1);
+
+	}
+	return(0);
+
+}
