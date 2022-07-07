ee1bc60e412f53c1bbbf1e52762d1308df4c1a3f
Melaku <melakuwondwossen111@gmail.com>
2022-07-08 02:18:32 +0300

    Initial commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,9 +6,6 @@
 *  (* a blank line
 *   *@name: string to print.
 *    *@f: this a function pointer, that recieves a string.
-*    * Description: this function prints a name)?
-*    (* section header: the header of this function is function_pointers.h)*
-*    * Return: this is a void function no return
 */
 
 void print_name(char *name, void (*f)(char *))
