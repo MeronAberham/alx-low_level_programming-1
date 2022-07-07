e71afff7358f83028cec78526725e494c8ad4400
Melaku <melakuwondwossen111@gmail.com>
2022-07-08 02:13:52 +0300

    initial commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name.
+ * print_name - prints a name
  * @name: input name.
  * @f: function pointer.
  *
@@ -12,4 +12,3 @@ void print_name(char *name, void (*f)(char *))
 	if (name && f)
 		f(name);
 }
-
